a026e67d0f7d9bd2d6c230e1de4022a73c9f69bb
Marco Afonso <mafonso333@gmail.com>
2012-11-05 23:26:38 +0000

    Added english language file for account management

---
+++ application/language/english/account_lang.php
@@ -0,0 +1,13 @@
+<?
+
+$lang['error.account.load'] =       "User account %s does not exists! <a href=\"%s\">Back</a>";
+$lang['error.email.invalid'] =      "Invalid email address";
+$lang['error.username.invalid'] =   "Invalid username";
+
+$lang['info.account.save'] =        "User account was saved";
+$lang['info.password.save'] =       "User password was saved";
+$lang['info.creategroup.save'] =    "Role %s was saved";
+$lang['info.addtogroup.save'] =     "The account was added to role %s";
+$lang['info.groups.delete'] =       "The account was removed from all roles";
+$lang['info.groups.save'] =         "The account was added to the roles %s";
+$lang['info.accounts.delete'] =     "The user accounts were deleted!";
