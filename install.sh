92813b7d3f451d741fd845787542f85ae933085b
taviroquai <mafonso333@gmail.com>
2013-06-08 19:25:52 +0100

    Bugfix: copy data.dist recursive

--- install.sh
+++ install.sh
@@ -76,7 +76,7 @@ echo "Installing MapIgniter..."
 cp index.dist.php index.php
 cp htaccess.dist .htaccess
 cp application/config/config.dist.php application/config/config.php
-cp data.dist data
+cp -R data.dist data
 chown -R www-data application/config
 mkdir data/cache
 chown -R www-data data
